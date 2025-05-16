307c042016f46460264ccd86c78de3d28a01e010
Bogdan <bogdan.duta100@gmail.com>
2025-05-16 10:47:41 +0100

    http code moved to handler

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"context"
+	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
+	"encoding/json"
+	"newtodoapp/models"
 	"newtodoapp/server"
+
+	_ "net/http/pprof"
 
 	"github.com/google/uuid"
 )
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	server := initialize()
 	go actor()
 	log.Fatal(http.ListenAndServe(":5000", server.Handler))
@@ -48,11 +57,11 @@ func traceMiddleware(handler http.Handler) http.Handler {
 	})
 }
 
-type request struct {
-	request     *http.Request
-	response    http.ResponseWriter
-	signalChan  chan bool
-	requestType string
+type actorRequest struct {
+	request      models.ToDoItem
+	responseChan chan map[string]string
+	signalChan   chan bool
+	requestType  string
 }
 
 const (
@@ -63,61 +72,165 @@ const (
 	listRequest   = "list"
 )
 
-var requestChan = make(chan request, 10)
+var requestChan = make(chan actorRequest, 10)
+
+const jsonContentType = "application/json"
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
+
+	responseChan := make(chan map[string]string)
 	signalChan := make(chan bool)
-	requestChan <- request{
-		request:     r,
-		response:    w,
-		signalChan:  signalChan,
-		requestType: getRequest,
+	requestChan <- actorRequest{
+		responseChan: responseChan,
+		signalChan:   signalChan,
+		requestType:  getRequest,
 	}
+	items := <-responseChan
 	<-signalChan
+
+	traceID := r.Context().Value("traceID").(string)
+	w.Header().Set("Content-Type", jsonContentType)
+
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(items); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
 }
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
+	traceID := r.Context().Value("traceID").(string)
+	w.Header().Set("Content-Type", jsonContentType)
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
+		return
+	}
+
+	var input models.ToDoItem
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Invalid request payload", "traceID": "` + traceID + `"}`))
+		return
+	}
+
+	responseChan := make(chan map[string]string)
 	signalChan := make(chan bool)
-	requestChan <- request{
-		request:     r,
-		response:    w,
-		signalChan:  signalChan,
-		requestType: createRequest,
+	requestChan <- actorRequest{
+		request:      input,
+		responseChan: responseChan,
+		signalChan:   signalChan,
+		requestType:  createRequest,
 	}
 	<-signalChan
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`{"message": "ToDo item created successfully", "traceID": "` + traceID + `"}`))
 }
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
+	traceID := r.Context().Value("traceID").(string)
+	w.Header().Set("Content-Type", jsonContentType)
+
+	if r.Method != http.MethodPut {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
+		return
+	}
+
+	var input models.ToDoItem
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Invalid request payload", "traceID": "` + traceID + `"}`))
+		return
+	}
+
 	signalChan := make(chan bool)
-	requestChan <- request{
-		request:     r,
-		response:    w,
+	requestChan <- actorRequest{
+		request:     input,
 		signalChan:  signalChan,
 		requestType: updateRequest,
 	}
 	<-signalChan
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "ToDo item updated successfully", "traceID": "` + traceID + `"}`))
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	traceID := r.Context().Value("traceID").(string)
+	w.Header().Set("Content-Type", jsonContentType)
+
+	if r.Method != http.MethodDelete {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
+		return
+	}
+
+	var input models.ToDoItem
+
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Invalid request payload", "traceID": "` + traceID + `"}`))
+		return
+	}
+
 	signalChan := make(chan bool)
-	requestChan <- request{
-		request:     r,
-		response:    w,
+	requestChan <- actorRequest{
+		request:     input,
 		signalChan:  signalChan,
 		requestType: deleteRequest,
 	}
 	<-signalChan
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "ToDo item deleted successfully", "traceID": "` + traceID + `"}`))
 }
 
 func listHandler(w http.ResponseWriter, r *http.Request) {
-	signalChan := make(chan bool)
-	requestChan <- request{
-		request:     r,
-		response:    w,
-		signalChan:  signalChan,
-		requestType: listRequest,
+	traceID := r.Context().Value("traceID").(string)
+
+	tmpl, err := template.ParseFiles("templates/todos.gohtml")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "error parsing template ` + err.Error() + `", "traceID": "` + traceID + `"}`))
+		return
 	}
+
+	responseChan := make(chan map[string]string)
+	signalChan := make(chan bool)
+	requestChan <- actorRequest{
+		responseChan: responseChan,
+		signalChan:   signalChan,
+		requestType:  listRequest,
+	}
+	items := <-responseChan
 	<-signalChan
+
+	var todoItems []models.ToDoItem
+	for name, description := range items {
+		todoItems = append(todoItems, models.ToDoItem{
+			Name:        name,
+			Description: description,
+		})
+	}
+
+	err = tmpl.Execute(w, todoItems)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "internal server error: ` + err.Error() + `", "traceID": "` + traceID + `"}`))
+		return
+	}
+
 }
 
 func actor() {
@@ -128,17 +241,18 @@ func actor() {
 		}
 		switch req.requestType {
 		case getRequest:
-			server.GetItems(req.response, req.request)
+			req.responseChan <- server.GetItems()
 		case createRequest:
-			server.CreateItem(req.response, req.request)
+			server.UpdateItem(req.request.Name, req.request.Description)
 		case updateRequest:
-			server.UpdateItem(req.response, req.request)
+			server.UpdateItem(req.request.Name, req.request.Description)
 		case deleteRequest:
-			server.DeleteItem(req.response, req.request)
+			server.DeleteItem(req.request.Name)
 		case listRequest:
-			server.ListItems(req.response, req.request)
+			req.responseChan <- server.GetItems()
 		}
 		req.signalChan <- true
+		close(req.responseChan)
 		close(req.signalChan)
 	}
 }
--- server/server.go
+++ server/server.go
@@ -1,128 +1,15 @@
 package server
 
-import (
-	"encoding/json"
-	"html/template"
-	"net/http"
+var Items = make(map[string]string)
 
-	"newtodoapp/models"
-)
-
-const jsonContentType = "application/json"
-
-var items = make(map[string]string)
-
-func CreateItem(w http.ResponseWriter, r *http.Request) {
-	traceID := r.Context().Value("traceID").(string)
-	w.Header().Set("Content-Type", jsonContentType)
-
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	var input models.ToDoItem
-
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "Invalid request payload", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	items[input.Name] = input.Description
-
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(`{"message": "ToDo item created successfully", "traceID": "` + traceID + `"}`))
+func GetItems() map[string]string {
+	return Items
 }
 
-func GetItems(w http.ResponseWriter, r *http.Request) {
-	traceID := r.Context().Value("traceID").(string)
-	w.Header().Set("Content-Type", jsonContentType)
-
-	if r.Method != http.MethodGet {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(items); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+func UpdateItem(name string, description string) {
+	Items[name] = description
 }
 
-func UpdateItem(w http.ResponseWriter, r *http.Request) {
-	traceID := r.Context().Value("traceID").(string)
-	w.Header().Set("Content-Type", jsonContentType)
-
-	if r.Method != http.MethodPut {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	var input models.ToDoItem
-
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "Invalid request payload", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	items[input.Name] = input.Description
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "ToDo item updated successfully", "traceID": "` + traceID + `"}`))
-}
-
-func DeleteItem(w http.ResponseWriter, r *http.Request) {
-	traceID := r.Context().Value("traceID").(string)
-	w.Header().Set("Content-Type", jsonContentType)
-
-	if r.Method != http.MethodDelete {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte(`{"error": "Method not allowed", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	var input models.ToDoItem
-
-	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "Invalid request payload", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	delete(items, input.Name)
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "ToDo item deleted successfully", "traceID": "` + traceID + `"}`))
-}
-
-func ListItems(w http.ResponseWriter, r *http.Request) {
-	traceID := r.Context().Value("traceID").(string)
-
-	tmpl, err := template.ParseFiles("templates/todos.gohtml")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error": "error parsing template ` + err.Error() + `", "traceID": "` + traceID + `"}`))
-		return
-	}
-
-	var todoItems []models.ToDoItem
-	for name, description := range items {
-		todoItems = append(todoItems, models.ToDoItem{
-			Name:        name,
-			Description: description,
-		})
-	}
-
-	err = tmpl.Execute(w, todoItems)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(`{"error": "internal server error: ` + err.Error() + `", "traceID": "` + traceID + `"}`))
-		return
-	}
-
+func DeleteItem(name string) {
+	delete(Items, name)
 }
